a332c5aa4b30662b8f4a40ef27b68c5b109b0ca0
FionaStrasser0 <c00312667@setu.ie>
2025-09-01 19:29:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+	<script>
+			function setupTabs () {
+				document.querySelectorAll(".tabs__button").forEach(button => {
+					button.addEventListener("click" , () => {
+						const sideBar = button.parentElement;
+						const tabsContainer = sidebar.parentElement;
+						const tabsNumber = button.dataset.forTab;
+						const tabToActivate = tabsContainer.querySelector('.tabs__content[data-tab="${tabNumber}"]);
+																		  
+						console.log(sideBar);
+						console.log(tabsContainer);
+						console.log(tabsNumber);
+						console.log(tabToActivate);
 
+						sideBar.querySelectorAll(".tabs__button").forEach(button => {
+							button.classList.remove("tabs__button--active");
+						});
+
+						tabContainer.querySelectorAll(".tabs__content").forEach(tab => {
+							tab.classList.remove("tabs__content--active");
+						});
+						button.classList.add("tabs__button--active");
+						tabToActivate.classList.add("tabs__content--active");
+					});
+				});
+			}
+			document.addEventListener("DOMcontentLoaded"), () => {
+				setupTabs(); 
+
+				document.querySelectorAll(".tabs").forEach(tabsContainer => {
+					tabsContainer.querySelector(".tabs__sidebar .tabs__button").click():
+				});
+			});
+		</script>
